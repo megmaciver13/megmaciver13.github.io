7a22579cb2693840868163925de486d69c750cd0
Meg MacIver <megmaciver13@gmail.com>
2018-01-02 12:01:14 -0500

    js on more info links working

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 function portfolio () {
-  var whoMore = document.querySelector('.who-more')
-  var whereMore = document.querySelector('.where-more')
-  var whatMore = document.querySelector('.what-more')
-  var headerText = document.querySelector('.header-text')
+  var whoMore = document.querySelector('.who')
+  var whereMore = document.querySelector('.where')
+  var whatMore = document.querySelector('.what')
   var moreWho = document.querySelector('.more-who')
-  var button = document.querySelector('.button')
+  var moreWhere = document.querySelector('.more-where')
+  var moreWhat = document.querySelector('.more-what')
+
+  var showWho = () => {
+    console.log('more who info')
+    moreWho.style.display = 'inline'
+    moreWhere.style.display = 'none'
+    moreWhat.style.display = 'none'
+  }
+
+  var showWhere = () => {
+    console.log('more where info')
+    moreWho.style.display = 'none'
+    moreWhere.style.display = 'inline'
+    moreWhat.style.display = 'none'
+  }
+
+  var showWhat = () => {
+    console.log('more what info')
+    moreWho.style.display = 'none'
+    moreWhere.style.display = 'none'
+    moreWhat.style.display = 'inline'
+  }
 
   whoMore.addEventListener('click', showWho)
-
-  function showWho () {
-    moreWho.innerHTML = '<p>hello world</p>'
-  }
+  whereMore.addEventListener('click', showWhere)
+  whatMore.addEventListener('click', showWhat)
 }
 
 portfolio()
